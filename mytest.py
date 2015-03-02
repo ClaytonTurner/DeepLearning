2d9b65b2021e7c769b5b45aba2e4c173c83284b9
Clayton Turner <caturner3@g.cofc.edu>
2015-03-02 15:19:09 -0500

    Backing up files after power outage

--- mytest.py
+++ mytest.py
@@ -1,3 +1,7 @@
+import theano
+import numpy as np
+import scipy.sparse as sp
+from theano import sparse
 import denoisingAutoencoder as dA
 import SdA
 import os
@@ -27,3 +31,46 @@ def run():
     batch_size=1
     '''
     SdA.test_SdA()
+
+def readSparse():
+    # Ref: http://www.deeplearning.net/software/theano/library/sparse/index.html#libdoc-sparse
+    
+    # Names of input files output by cTAKES
+    attributesString = "attributes.txt"
+    instancesString = "instance.txt"
+    dataString = "data.txt"
+    
+    # Start at repo name: here it's DeepLearning
+    # Change directory to DeepLearning/datasets/example_notes
+    os.chdir(os.path.join("datasets","example_notes"))
+    
+    # Read in data files
+    attrFile = open(attributesString,"r")
+    attributes = attrFile.readlines()
+    attrFile.close()
+    instanceFile = open(instancesString,"r")
+    instance = instanceFile.readlines()
+    instanceFile.close()
+    dataFile = open(dataString,"r")
+    dataLines = dataFile.readlines()
+    dataFile.close()
+    
+    # 1 to 1 relationship with data and indices
+    # data = [a,b,c]
+    # indices = [x,y,z]
+    # datapoint a is on row x
+    dataArray = []
+    data = np.asarray(dataArray)
+    indicesArray = [] 
+    indices = np.asarray(indicesArray)
+    indptrArray = []
+    indptr = np.asarray(indptrArray)
+    
+    # For shape of array
+    rows = len(instance)
+    cols = len(attributes)
+    
+    m = sp.csc_matrix((data,indices,indptr), shape=(rows,cols))
+    print m.toarray()
+    
+readSparse()
